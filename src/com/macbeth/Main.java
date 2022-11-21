2211d820c1b5a39f8fd7a7200cd2f9f924538a05
n-f9 <ndferguson@protonmail.com>
2022-11-21 11:00:30 -0500

    fds

--- src/com/macbeth/Main.java
+++ src/com/macbeth/Main.java
@@ -1,7 +1,11 @@
 package com.macbeth;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     
     System.out.println("███    ███  █████   ██████ ██████  ███████ ████████ ██   ██ ");
     System.out.println("████  ████ ██   ██ ██      ██   ██ ██         ██    ██   ██ ");
@@ -9,5 +13,13 @@ public class Main {
     System.out.println("██  ██  ██ ██   ██ ██      ██   ██ ██         ██    ██   ██ ");
     System.out.println("██      ██ ██   ██  ██████ ██████  ███████    ██    ██   ██ ");
     System.out.println();
+
+    // Scanner sc = new Scanner(new File("lines.txt"));
+    // System.out.println(sc.nextLine());
+
+    boolean moonMode = false;
+    System.out.println("Which mode would you like to try?");
+    
+
   }
 }
