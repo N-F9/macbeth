14eed412e9cd952efcce0e3970150ad2166b2f35
n-f9 <ndferguson@protonmail.com>
2022-11-21 11:09:49 -0500

    fd

--- src/com/macbeth/Main.java
+++ src/com/macbeth/Main.java
@@ -17,7 +17,7 @@ public class Main {
     // Scanner sc = new Scanner(new File("lines.txt"));
     // System.out.println(sc.nextLine());
 
-    boolean moonMode = false;
+    int mode = 0; // 0 is moon mode, 1 is regular
     System.out.println("Which mode would you like to try?");
     
 
