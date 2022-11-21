cbcedb1b663e470b8df37106e8518e06a1be5393
n-f9 <ndferguson@protonmail.com>
2022-11-21 10:48:27 -0500

    fdsa

--- src/com/macbeth/Main.java
+++ src/com/macbeth/Main.java
@@ -2,6 +2,12 @@ package com.macbeth;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Macbeth!");
+    
+    System.out.println("███    ███  █████   ██████ ██████  ███████ ████████ ██   ██ ");
+    System.out.println("████  ████ ██   ██ ██      ██   ██ ██         ██    ██   ██ ");
+    System.out.println("██ ████ ██ ███████ ██      ██████  █████      ██    ███████ ");
+    System.out.println("██  ██  ██ ██   ██ ██      ██   ██ ██         ██    ██   ██ ");
+    System.out.println("██      ██ ██   ██  ██████ ██████  ███████    ██    ██   ██ ");
+    System.out.println();
   }
 }
